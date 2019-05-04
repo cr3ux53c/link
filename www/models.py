207e27cc178b77e5015fa480d78e22982df1b4bf
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-05-04 20:42:59 +0900

    LINK 모델 생성

--- www/models.py
+++ www/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Link(models.Model):
+    class Meta:
+        verbose_name = '링크'
+        verbose_name_plural = '링크'
+
+    href = models.URLField()
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
