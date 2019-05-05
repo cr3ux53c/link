33232498254efa00f3989a34b38664a824af0b8c
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-05-05 18:05:18 +0900

    Link Admin 페이지 출력

--- www/admin.py
+++ www/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from www.models import Link
 
-# Register your models here.
 
-admin.site.register(Link)
+# Register your models here.
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'path', 'href')
+
+
+admin.site.register(Link, LinkAdmin)
