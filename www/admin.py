6380e3f0c9b55f70cd45f82966a990aa25a928eb
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-05-05 19:08:51 +0900

    admin 페이지 출력 항목 수정

--- www/admin.py
+++ www/admin.py
@@ -5,6 +5,7 @@ from www.models import Link, Metadata
 # Register your models here.
 class LinkAdmin(admin.ModelAdmin):
     list_display = ('name', 'path', 'href')
+    fields = ['href', 'path', 'name']
 
 
 admin.site.register(Link, LinkAdmin)
